ccc202239fb4a1eb76883f518ba842cedfda49f1
nipuns11 <nipunsharma@nipuns-Mac-mini.local>
2015-10-18 01:39:16 -0400

    Stop watch app in swift
    Stop watch app in swift

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -10,6 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var counter = 0
+    var timer = NSTimer()
+    var time = 00
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    @IBAction func Go(sender: AnyObject) {
+        
+    }
+    func increaseTimer(){
+        timerLabel.text = "String\(counter)"
+        time++
+        timerLabel.text = "\(time)"
+    }
+    
+    @IBAction func play(sender: AnyObject) {
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(01, target: self, selector:Selector("increaseTimer"), userInfo: nil, repeats: true)
+        
+    }
+    @IBAction func pause(sender: AnyObject) {
+        timer.invalidate()
+        
+    }
+    @IBAction func reset(sender: AnyObject) {
+        timer.invalidate()
+        time = 0
+        timerLabel.text = "0"
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
